2ce4f3ea6c2107e5db1b8fc2b9445d27cd4d45cc
Bas Bossink <bas.bossink@gmail.com>
2013-08-05 23:29:56 +0200

    Finish exercise 5

---
+++ src/ch03/write_out.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  cout << "this is a bool: " << boolalpha << true << endl;
+  cout << "this is a char: " << 'a' << endl;
+  cout << "this is an int: " << 37 << endl;
+  cout << "this is a double: " << 37.42 << endl;
+  cout << "this is a string: " << "Fred is a happy Flinstone." << endl;
+}
