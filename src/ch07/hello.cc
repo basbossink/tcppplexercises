3a057eb14e952b7658f50d3c06c878769e45ac80
Bas Bossink <bas.bossink@gmail.com>
2013-08-18 13:49:59 +0200

    Initial version of X.6.2

---
+++ src/ch07/hello.cc
@@ -0,0 +1,47 @@
+// Copyright (C) 2013 Bas Bossink <bas.bossink@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <thread>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+struct IntervalWriter {
+  IntervalWriter(const string& stringToWrite) : payLoad(stringToWrite) {}
+  void operator()() {
+    constexpr int OneSecondInMilliSeconds = 1000; 
+    for(;;) {
+      if(duration_cast<milliseconds>(high_resolution_clock::now() - lastHelloTime).count() >= OneSecondInMilliSeconds) {
+        cout << payLoad << endl;
+        lastHelloTime = high_resolution_clock::now();
+      }
+    }
+  }
+  private:
+    high_resolution_clock::time_point lastHelloTime = high_resolution_clock::now(); 
+    string payLoad;
+};
+
+int main() {
+  IntervalWriter helloWriter { "Hello" };
+  IntervalWriter worldWriter { "world!" };
+  thread helloThread { helloWriter };
+  thread worldThread { worldWriter };
+  helloThread.join();
+  worldThread.join();
+}
