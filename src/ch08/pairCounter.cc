83e0f1902ac3037c7f0516510a10854eb47f6054
Bas Bossink <bas.bossink@gmail.com>
2013-12-23 00:12:20 +0100

    Finished exercise X.8.12.

---
+++ src/ch08/pairCounter.cc
@@ -0,0 +1,46 @@
+// Copyright (C) 2013 Bas Bossink <bas.bossink@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <string>
+#include <algorithm>
+#include "pairCounter.h"
+#include <cstring>
+
+namespace tcppplexercises {
+  int countPairs(std::string pair, std::string searchString) {
+    int returnValue = 0;
+    auto start = searchString.begin();
+    while((start = std::find(start, searchString.end(), pair[0])) != searchString.end()) {
+      auto next = start+1;
+      if(*next == pair[1]) {
+        returnValue++;
+      }
+      start++;
+    }
+    return returnValue;
+  }
+
+  int countPairs(const char* pair, const char* searchString) {
+    int returnValue = 0;
+    auto start = searchString;
+    auto end = start+strlen(searchString)+1;
+    while((start = std::find(start, end, pair[0])) != end) {
+      if(*(++start) == pair[1]) {
+        returnValue++;
+      }
+    }
+    return returnValue;
+  }
+}
---
+++ test/ch08/countPairsTest.cc
@@ -0,0 +1,76 @@
+// Copyright (C) 2013 Bas Bossink <bas.bossink@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+#include <string>
+#include "../../src/ch08/pairCounter.h"
+
+using namespace std;
+using namespace tcppplexercises;
+
+namespace test {
+  namespace  {
+    TEST(X_8_12, PairCounterTestSimple) {
+      string testString{ "xabaacbaxabb" };
+      string pair{ "ab" };
+      ASSERT_EQ(2, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestSimpleSingle) {
+      string testString{ "xaba" };
+      string pair{ "ab" };
+      ASSERT_EQ(1, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestSimpleNone) {
+      string testString{ "xaca" };
+      string pair{ "ab" };
+      ASSERT_EQ(0, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestEmptyString) {
+      string testString{ "" };
+      string pair{ "ab" };
+      ASSERT_EQ(0, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestThree) {
+      string testString{ "iabbaabioabaeueo" };
+      string pair{ "ab" };
+      ASSERT_EQ(3, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestCStringSimple) {
+      const char* testString{ "xabaacbaxabb" };
+      const char* pair{ "ab" };
+      ASSERT_EQ(2, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestSimpleSingleCString) {
+      const char* testString{ "xaba" };
+      const char* pair{ "ab" };
+      ASSERT_EQ(1, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestSimpleNoneCString) {
+      const char* testString{ "xaca" };
+      const char* pair{ "ab" };
+      ASSERT_EQ(0, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestEmptyStringCString) {
+      const char* testString{ "" };
+      const char* pair{ "ab" };
+      ASSERT_EQ(0, countPairs(pair, testString));      
+    }
+    TEST(X_8_12, PairCounterTestThreeCString) {
+      const char* testString{ "iabbaabioabaeueo" };
+      const char* pair{ "ab" };
+      ASSERT_EQ(3, countPairs(pair, testString));      
+    }
+  }
+}
